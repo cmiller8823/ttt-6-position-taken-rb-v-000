cb1573cebec05eba4d9fe24d6376bb1507f99e38
Chris Miller <chris.miller8823@gmail.com>
2019-12-26 19:01:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if index == "" || index == " " || index == nil
-    return false
-  elsif board[index] == "X" || board[index] == "O"
+  if board[index] == "X" || board[index] == "O"
     return true
+  elsif index == "" || index == " " || index == nil
+    return false
   end
 end
