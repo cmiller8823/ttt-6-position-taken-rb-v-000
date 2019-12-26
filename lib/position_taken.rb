48bdf7b8cd6aaa42cd8101a071334fc0132591ac
Chris Miller <chris.miller8823@gmail.com>
2019-12-26 19:00:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@
 def position_taken?(board, index)
   if index == "" || index == " " || index == nil
     return false
-  elsif board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O"
     return true
 end
